f457b89f8baa60a0354909a099b053386977bb78
Raul Gomez <rgomez@tec.mx>
2025-07-08 18:49:40 -0600

    Started Chapter 3 testing file

--- scripts/Chapter 3/test.R
+++ scripts/Chapter 3/test.R
@@ -3,8 +3,9 @@
 library("tidyverse")
 library("here")
 library("cowplot")
+library("krulRutils")
+library("ISLR2")
 
 # Load the Advertising dataset
 
 advertising_tbl <- read_csv(here("data", "Advertising.csv"))
-fragile_advertising_tbl <- read_csv("data/Advertising.csv")
