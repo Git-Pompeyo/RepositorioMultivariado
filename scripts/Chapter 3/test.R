479b380d7d6f9df17b539be786df4eca032e8276
Raul Gomez <rgomez@tec.mx>
2025-07-12 11:29:43 -0600

    Experimenting with Chapter 3

--- scripts/Chapter 3/test.R
+++ scripts/Chapter 3/test.R
@@ -8,4 +8,5 @@ library("ISLR2")
 
 # Load the Advertising dataset
 
-advertising_tbl <- read_csv(here("data", "Advertising.csv"))
+advertising_tbl <- read_csv(here("data", "Advertising.csv")) %>%
+  select(-1)
