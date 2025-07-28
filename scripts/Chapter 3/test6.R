84c9f05b763e38dce302f3413c23345a14583c1f
Raul Gomez <rgomez@tec.mx>
2025-07-27 21:54:55 -0600

    Added kmode function

--- scripts/Chapter 3/test6.R
+++ scripts/Chapter 3/test6.R
@@ -14,3 +14,34 @@ airquality_tbl <- airquality %>%
     Month = factor(Month, levels = 1:12, labels = month.abb[1:12]),
     Day = factor(Day)
   )
+
+data <- iris %>%
+  as_tibble %>%
+  select(Species, Sepal.Length)
+
+dt <- data %>%
+  dplyr::group_by(Species) %>%
+  dplyr::summarise(
+    mean = mean(Sepal.Length),
+    lci = t.test(Sepal.Length, conf.level = 0.95)$conf.int[1],
+    uci = t.test(Sepal.Length, conf.level = 0.95)$conf.int[2]
+  )
+
+
+my_plot <- dt %>%
+  ggplot(aes(x = Species, y = mean, ymin = lci, ymax = uci)) +
+  geom_point(
+    color = c_pal("C red"),
+    size = 3
+  ) +
+  geom_errorbar(
+    width = 0.3,
+    color = c_pal("C red"),
+    linewidth = 1
+  ) +
+  labs(
+    title = "Confidence Intervals test",
+    x = "Species",
+    y = " Sepal Lenght"
+  ) +
+  theme_krul()
