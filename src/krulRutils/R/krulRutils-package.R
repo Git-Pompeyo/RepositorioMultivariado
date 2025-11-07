7cf1d7949b85d90e278f9f1627d3972f33057eb8
Raul Gomez <rgomez@tec.mx>
2025-11-06 22:25:34 -0600

    Started working on krulRutils package

---
+++ src/krulRutils/R/krulRutils-package.R
@@ -0,0 +1,12 @@
+#' Utility functions for MA2003B
+#'
+#' Internal helper package used across the MA2003B course materials.
+#'
+#' @docType package
+#' @name krulRutils
+#' @import dplyr
+#' @import ggplot2
+#' @import tidyr
+#' @import GGally
+#' @importFrom magrittr %>%
+NULL
